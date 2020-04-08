ffec7253c3112d9f63f54e8bc2b780cdb1bf8bc1
souravtecken <souravtecken@gmail.com>
2020-04-08 18:46:25 +0530

    Added check for repeating terms in a minterm

--- js/3-var.js
+++ js/3-var.js
@@ -1,50 +1,36 @@
-var finalExpression = '';
+let finalExpression = '';
 function submit(clickedButton)
 {
-    var inputBox = document.getElementById("inputExpression");
+    const inputBox = document.getElementById("inputExpression");
+    let errorMessage = "";
+    let hasFoundError = false;
     finalExpression = inputBox.value;
-    console.log(finalExpression);
-    var splitFinalExpression = finalExpression.split("");
-    if (splitFinalExpression[splitFinalExpression.length-1] == "+") 
-    {
-    alert("Invalid expression ending with a logical operator. Please input a valid expression");
-    }
+    
+    if (finalExpression[finalExpression.length - 1] == "+"){
+        errorMessage = "Invalid expression ending with a logical operator. Please input a valid expression";
+        hasFoundError = true;
+    }        
 
-    var splitPlus = finalExpression.split("+");
-    console.log(splitPlus);
-    for (let i = 0; i < splitPlus.length; i++)
+    const minterms = finalExpression.split("+");
+    for (let minterm of minterms)
     {
-        var splitTerm = [];
-        splitTerm = splitPlus[i].split("");
-        console.log(splitTerm)
-        for (let j = 0; j < splitTerm.length-2; j++)
-        {   
-            console.log(splitTerm[j])
-            for (let k = j+1; k < splitTerm.length-1; k++ )
-            {   
-                console.log(splitTerm[k])
-                if (splitTerm[j] == splitTerm[k])
-                {
-                    if (splitTerm[k] != "'")
-                    {
-                        alert("Invalid expression as the same Boolean or complement Boolean value are used in the same term");
-                        splitTerm = [];
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                
-                }
-                else
-                {
-                    continue;
-                }
-            }
+        let tokenCounter = new Object();
+        for(let token of minterm){
+            if(token == `'`)
+                continue;
+            if(!Object.keys(tokenCounter).includes(token))
+                tokenCounter[token] = 0;
+            tokenCounter[token] += 1;
+            if(tokenCounter[token] > 1){
+                errorMessage = "Invalid expression as the same Boolean or complement Boolean value are used in the same term";
+                hasFoundError = true;
+                break;
+            }                
         }
-        splitTerm = [];
+        if(hasFoundError)
+            break;
     }
+    
+    if(hasFoundError)
+        alert(errorMessage);
 }
-
-
-
